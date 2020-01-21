626e38754bd11806811adc7058e51418fffe7ccf
parndt <arndt.philipp@gmx.de>
2020-01-21 17:56:59 +0100

    Adjusted webservice for new DB schema

--- src/main/scala/de/htwBerlin/ai/inews/data/ArticleHitReader.scala
+++ src/main/scala/de/htwBerlin/ai/inews/data/ArticleHitReader.scala
@@ -8,27 +8,29 @@ import scala.util.Try
 
 object ArticleHitReader extends HitReader[Article] {
   override def read(hit: Hit): Try[Article] = {
-    val publishedTime = hit.sourceAsMap("published_time") match {
-      case Some(time: String) => DateTime.parse(time).getMillis
-      case _ => 0
-    }
 
     Try(Article(
       hit.id,
       hit.sourceAsMap("authors").asInstanceOf[List[String]],
-      DateTime.parse(hit.sourceAsMap("crawl_time").toString).getMillis,
-      Seq(), // TODO departments
+      hit.sourceAsMap("crawlTime") match {
+        case time: String => DateTime.parse(time).getMillis
+        case _ => 0
+      },
+      hit.sourceAsMap("departments").asInstanceOf[List[String]],
       hit.sourceAsMap("description").toString,
-      hit.sourceAsMap("image_links").asInstanceOf[List[String]],
+      hit.sourceAsMap("imageLinks").asInstanceOf[List[String]],
       hit.sourceAsMap("intro").toString,
       hit.sourceAsMap("keywords").asInstanceOf[List[String]],
       hit.sourceAsMap("lemmas").asInstanceOf[List[String]],
       hit.sourceAsMap("links").asInstanceOf[List[String]],
-      hit.sourceAsMap("long_url").toString,
-      Seq(), // TODO mostRelevantLemmas
+      hit.sourceAsMap("longUrl").toString,
+      hit.sourceAsMap("mostRelevantLemmas").asInstanceOf[List[String]],
       hit.sourceAsMap("newsSite").toString,
-      publishedTime,
-      hit.sourceAsMap("reading_time").asInstanceOf[Int],
+      hit.sourceAsMap("publishedTime") match {
+        case time: String => DateTime.parse(time).getMillis
+        case _ => 0
+      },
+      hit.sourceAsMap("readingTime").asInstanceOf[Int],
       hit.sourceAsMap("text").toString,
       hit.sourceAsMap("title").toString
     ))
--- src/main/scala/de/htwBerlin/ai/inews/data/ArticleService.scala
+++ src/main/scala/de/htwBerlin/ai/inews/data/ArticleService.scala
@@ -43,7 +43,7 @@ class ArticleService()(implicit executionContext: ExecutionContext) {
       case _ =>
     }
 
-    if (query.departments.nonEmpty) {
+    if (query.departments.exists(!_.isEmpty)) {
       request = request.bool(
         should(
           {
@@ -55,7 +55,7 @@ class ArticleService()(implicit executionContext: ExecutionContext) {
       )
     }
 
-    if (query.newspapers.nonEmpty) {
+    if (query.newspapers.exists(!_.isEmpty)) {
       request = request.bool(
         should(
           {
