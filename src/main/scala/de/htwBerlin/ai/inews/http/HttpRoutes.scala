620a6e1cca4d87d8d66b21ab944233765fc997e0
katharinakoslowski <s0561010@htw-berlin.de>
2021-02-08 12:45:24 +0100

    activated authors route

--- src/main/scala/de/htwBerlin/ai/inews/http/HttpRoutes.scala
+++ src/main/scala/de/htwBerlin/ai/inews/http/HttpRoutes.scala
@@ -2,9 +2,9 @@ package de.htwBerlin.ai.inews.http
 
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
-import de.htwBerlin.ai.inews.http.routes.{Analytics, Articles, Users}
+import de.htwBerlin.ai.inews.http.routes.{Analytics, Articles, Authors, Users}
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
-//import de.htwBerlin.ai.inews.author.AuthorService
+import de.htwBerlin.ai.inews.author.AuthorService
 import de.htwBerlin.ai.inews.data.ArticleService
 import de.htwBerlin.ai.inews.user.UserService
 
@@ -15,15 +15,15 @@ class HttpRoutes(implicit executionContext: ExecutionContext) {
   // TODO dependency injection
   private val articleService = new ArticleService()(executionContext)
   private val userService = new UserService(articleService)(executionContext)
-  //private val authorService = new AuthorService()(executionContext)
+  private val authorService = new AuthorService()(executionContext)
   private val articleRoute = new Articles(articleService)(executionContext)
   private val analyticsRoute = new Analytics(articleService)(executionContext)
   private val usersRoute = new Users(userService)(executionContext)
-  //private val authorsRoute = new Authors(authorService)(executionContext)
+  private val authorsRoute = new Authors(authorService)(executionContext)
 
   val route: Route = cors() {
     pathPrefix("api") {
-      //authorsRoute.route ~
+      authorsRoute.route ~
         usersRoute.route ~
       articleRoute.route ~
       analyticsRoute.route
